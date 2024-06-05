8f8f3e0ac23f7cefe2def17325ca34a6c5e42d30
FarithGodoy <131852885+FarithGodoy@users.noreply.github.com>
2024-06-05 19:47:18 +0200

    MAPA NO INTERACTIVO

--- lib/altres/mapa.dart
+++ lib/altres/mapa.dart
@@ -1,95 +1,133 @@
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last
+
 import 'package:flutter/material.dart';
+
 import '../model/user.dart';
-
-class MapScreen extends StatelessWidget {
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+class MapScreen extends StatefulWidget {
   final UserProfile user;
-
-  const MapScreen({super.key, required this.user});
+  MapScreen({super.key, required this.user});
 
   @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pantalla Mapa'),
+    return  Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          automaticallyImplyLeading: false,
+          title: Text(
+            'Mapa',
+            style: TextStyle(
+                  fontFamily: 'Comfortaa',
+                  fontSize: 22,
+                  letterSpacing: 0,
+                ),
+          ),
+          actions: [],
+          centerTitle: true,
+          elevation: 2,
+        ),
+        body: SafeArea(
+          top: true,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Expanded(
+                    child: Align(
+                      alignment: AlignmentDirectional(1, 0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Image.network(
+                          'https://cdn.discordapp.com/attachments/656490313405300736/1247960785137827900/image.png?ex=6661ed8d&is=66609c0d&hm=48f2841d011bf0063995a6709f6ad0e43396952793e43bb66f23896d55bc9cad&',
+                          width: double.infinity,
+                          height: 450,
+                          fit: BoxFit.fitHeight,
+                          alignment: Alignment(0, 1),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+  mainAxisSize: MainAxisSize.max,
+  children: [
+    Padding(
+      padding: EdgeInsets.all(24),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(6),
+        child: Image.network(
+          'https://cdn.discordapp.com/attachments/656490313405300736/1247965938985472060/image.png?ex=6661f259&is=6660a0d9&hm=82dcd022d7a9b513a1842ac9c47d8ac0e37f2cc1d0232f883520a110e68dcec2&',
+          width: 125,
+          height: 125,
+          fit: BoxFit.contain,
+        ),
       ),
-      body: const Center(
-        child: Text('Aqui va el mapa'),
-      ),
-      /*
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.white,
-        selectedItemColor: Colors.black,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(BarkMeet.step, color: Colors.black),
-            label: 'Inici',
+    ),
+    Padding(
+      padding: EdgeInsetsDirectional.fromSTEB(5, 0, 0, 0),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 4),
+            child: Text(
+              'Parc del Clot',
+              style: TextStyle(
+                    fontFamily: 'Readex Pro',
+                    fontSize: 18,
+                    letterSpacing: 0,
+                  ),
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(BarkMeet.message),
-            label: 'Chat',
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 4),
+            child: Text(
+              '800 m',
+              style: TextStyle(
+                    fontFamily: 'Readex Pro',
+                    fontSize: 15,
+                    letterSpacing: 0,
+                  ),
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(BarkMeet.map),
-            label: 'Mapa',
+          Text(
+            'C/ dels Escultors Claperós',
+            textAlign: TextAlign.start,
+            maxLines: 2,
+            style: TextStyle(
+                  fontFamily: 'Readex Pro',
+                  fontSize: 15,
+                  letterSpacing: 0,
+                ),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(BarkMeet.person),
-            label: 'Perfil',
+          Text(
+            '55, 63, Sant Mart',
+            style: TextStyle(
+                  fontFamily: 'Readex Pro',
+                  fontSize: 15,
+                  letterSpacing: 0,
+                ),
           ),
         ],
-        type: BottomNavigationBarType.fixed,
-        onTap: (int index) {
-          if (index == 0) {
-            Navigator.push(
-              context,
-              PageRouteBuilder(
-                pageBuilder: (context, animation, secondaryAnimation) =>
-                    MainPageAsync(user: user),
-                transitionsBuilder:
-                    (context, animation, secondaryAnimation, child) {
-                  return FadeTransition(
-                    opacity: animation,
-                    child: child,
-                  );
-                },
-              ),
-            );
-          } else if (index == 1) {
-            Navigator.push(
-              context,
-              PageRouteBuilder(
-                pageBuilder: (context, animation, secondaryAnimation) =>
-                    HomeChatScreen(user: user),
-                transitionsBuilder:
-                    (context, animation, secondaryAnimation, child) {
-                  return FadeTransition(
-                    opacity: animation,
-                    child: child,
-                  );
-                },
-              ),
-            );
-          } else if (index == 2) {
-            //do nothing
-          } else {
-            Navigator.push(
-              context,
-              PageRouteBuilder(
-                pageBuilder: (context, animation, secondaryAnimation) =>
-                    UserProfileScreen(user: user),
-                transitionsBuilder:
-                    (context, animation, secondaryAnimation, child) {
-                  return FadeTransition(
-                    opacity: animation,
-                    child: child,
-                  );
-                },
-              ),
-            );
-          }
-        },
       ),
-      */
+    ),
+  ],
+),
+            
+            
+          ],
+        ),
+      ),
     );
   }
 }
